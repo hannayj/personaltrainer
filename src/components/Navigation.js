3cde08e1b3b56e96465b63058c12cf970bc98694
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2020-03-01 14:26:00 +0200

    simple drawer nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,8 +6,68 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Menu, MenuItem } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+const useStyles = makeStyles({
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
+  });
 
 export default function Navigation(){
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (side, open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+        }
+
+        setState({ ...state, [side]: open });
+    };
+
+    const sideList = side => (
+        <div
+          className={classes.list}
+          role="presentation"
+          onClick={toggleDrawer(side, false)}
+          onKeyDown={toggleDrawer(side, false)}
+        >
+            <List component="nav" aria-label="main mailbox folders">
+                <ListItem button component={Link} to='/'>
+                <ListItemIcon>
+                    <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Customers" />
+                </ListItem>
+                <ListItem button component={Link} to='/trainings'>
+                <ListItemIcon>
+                    <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Trainings" />
+                </ListItem>
+            </List>
+
+        </div>
+      );
+
     const [anchorEl, setAnchorEl] = React.useState(null);
 
     const handleClick = event => {
@@ -23,29 +83,17 @@ export default function Navigation(){
         <div>
             <AppBar position="static">
                 <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleClick}>
+                <IconButton edge="start" color="inherit" aria-label="open-drawer" onClick={toggleDrawer('left', true)}>
                     <MenuIcon />
-                    <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                    >
-                    <MenuItem onClick={handleClose} component={Link} to='/'>
-                        Customers
-                    </MenuItem>
-                    
-                    <MenuItem onClick={handleClose} component={Link} to='/trainings'>
-                        Trainings
-                    </MenuItem>
-                    </Menu>
                 </IconButton>
                 <Typography variant="h6">
                     Personal trainer
                 </Typography>
                 </Toolbar>
             </AppBar>
+            <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+                {sideList('left')}
+            </Drawer>
         </div>
     );
 }
